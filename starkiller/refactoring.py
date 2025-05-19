c77d576be70583b954f0ca99f6eb778c216c0e4d
Vasily Negrebetskiy <41742529+kompoth@users.noreply.github.com>
2025-05-20 00:05:58 +0400

    V0.1.2 (#5)
    * New code actions:
      * Replace from import with module import
      * Replace module import with from import
      * Remove unnecessary import
    * Project structure and names refactoring

--- starkiller/refactoring.py
+++ starkiller/refactoring.py
@@ -1,30 +1,16 @@
-# ruff: noqa: N802
 """Utilities to change Python code."""
 
 from collections.abc import Generator
-from dataclasses import dataclass
 
 import parso
 
-
-@dataclass
-class EditPosition:
-    """Coordinate in source."""
-
-    line: int
-    char: int
+from starkiller.models import EditPosition, EditRange
 
 
-@dataclass
-class EditRange:
-    """Coordinates of source change."""
+def rename(source: str, rename_map: dict[str, str]) -> Generator[tuple[EditRange, str]]:
+    """Generate rename edits.
 
-    start: EditPosition
-    end: EditPosition
-
-
-def get_rename_edits(source: str, rename_map: dict[str, str]) -> Generator[tuple[EditRange, str]]:
-    """Generates source code changes to rename symbols.
+    Generates source code changes to rename names from rename_map. Doesn't affect imports.
 
     Args:
         source: Source code being refactored.
@@ -37,6 +23,10 @@ def get_rename_edits(source: str, rename_map: dict[str, str]) -> Generator[tuple
     for old_name, nodes in root.get_used_names().items():
         if old_name in rename_map:
             for node in nodes:
+                # Ignore imports
+                if node.search_ancestor("import_as_names", "import_from", "import_name"):
+                    continue
+
                 edit_range = EditRange(
                     start=EditPosition(
                         line=node.start_pos[0] - 1,
@@ -48,3 +38,40 @@ def get_rename_edits(source: str, rename_map: dict[str, str]) -> Generator[tuple
                     ),
                 )
                 yield (edit_range, rename_map[old_name])
+
+
+def strip_base_name(source: str, base_name: str, attrs: set[str]) -> Generator[tuple[EditRange, str]]:
+    """Generate base name strip edits for attribute calls.
+
+    Finds all base_name usages with attributes and generates edits stripping the base_name. Doesn't affect imports.
+
+    Args:
+        source: Source code being refactored.
+        base_name: Target name.
+        attrs: Attributes to be converted.
+
+    Yields:
+        EditRange and edit text.
+    """
+    root = parso.parse(source)
+    nodes = root.get_used_names().get(base_name, [])
+    for node in nodes:
+        operator_leaf = node.get_next_leaf()
+        if not isinstance(operator_leaf, parso.python.tree.Operator) or operator_leaf.value != ".":
+            continue
+        attr_leaf = operator_leaf.get_next_leaf()
+        if attr_leaf.value not in attrs:
+            continue
+
+        edit_range = EditRange(
+            start=EditPosition(
+                line=node.start_pos[0] - 1,
+                char=node.start_pos[1],
+            ),
+            end=EditPosition(
+                line=operator_leaf.end_pos[0] - 1,
+                char=operator_leaf.end_pos[1],
+            ),
+        )
+
+        yield (edit_range, "")
--- tests/test_refactoring.py
+++ tests/test_refactoring.py
@@ -1,17 +1,37 @@
 from parso import split_lines
 
-from starkiller.refactoring import EditRange, get_rename_edits
+from starkiller.refactoring import EditRange, rename, strip_base_name
 
-TEST_CASE = """
+RENAME_TEST_CASE = """
+from numpy import ndarray, dot
+
 a = ndarray([[1, 0], [0, 1]])
 b = ndarray([[4, 1], [2, 2]])
 print(dot(a, b))
 """
 
-EXPECTED_RESULT = """
+RENAME_EXPECTED_RESULT = """
+from numpy import ndarray, dot
+
 a = np.ndarray([[1, 0], [0, 1]])
 b = np.ndarray([[4, 1], [2, 2]])
 print(np.dot(a, b))
+"""
+
+ATTRS_TEST_CASE = """
+from numpy import ndarray, dot
+
+a = np.ndarray([[1, 0], [0, 1]])
+b = np.ndarray([[4, 1], [2, 2]])
+print(np.dot(a, b))
+"""
+
+ATTRS_EXPECTED_RESULT = """
+from numpy import ndarray, dot
+
+a = ndarray([[1, 0], [0, 1]])
+b = ndarray([[4, 1], [2, 2]])
+print(dot(a, b))
 """
 
 
@@ -27,5 +47,10 @@ def apply_inline_changes(source: str, changes: list[tuple[EditRange, str]]) -> s
 
 def test_rename() -> None:
     rename_map = {"ndarray": "np.ndarray", "dot": "np.dot"}
-    changes = list(get_rename_edits(TEST_CASE, rename_map))
-    assert apply_inline_changes(TEST_CASE, changes) == EXPECTED_RESULT
+    changes = list(rename(RENAME_TEST_CASE, rename_map))
+    assert apply_inline_changes(RENAME_TEST_CASE, changes) == RENAME_EXPECTED_RESULT
+
+
+def test_attrs_as_names() -> None:
+    changes = list(strip_base_name(ATTRS_TEST_CASE, "np", {"ndarray", "dot"}))
+    assert apply_inline_changes(ATTRS_TEST_CASE, changes) == ATTRS_EXPECTED_RESULT
